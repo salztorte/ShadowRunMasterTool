384ba7b6bb4172204035550b876a1fa49264f56c
salztorte <olli.franzen@gmx.de>
2016-11-04 10:53:31 +0100

    ..

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,25 +1,17 @@
-//import styles
-import 'grommet/scss/vanilla/index';
+import React, { PropTypes } from 'react';
 
-import { PropTypes } from 'react';
-
-import GromApp from 'grommet/components/App';
+import Page from 'grommet/components/App';
 import Header from 'grommet/components/Header';
 import Title from 'grommet/components/Title';
 
-import { routerActions } from 'react-router-redux';
+//import { routerActions } from 'react-router-redux';
 
-//import Toolbar from './Toolbar.jsx';
+import Toolbar from './Toolbar.jsx';
 //import SideMenu from './SideMenu.jsx';
-import { resourcesActions } from '../Resources';
+//import { resourcesActions } from '../Resources';
 
 import { appActions } from './AppActions';
 import { connector } from '../tools';
-
-//const leftToolbar = openMenu => <ToolbarButton onClick={openMenu}>
-//    <Icon icon="ion-navicon, material:md-menu"/>
-//</ToolbarButton>;
-//const toolbar = (title, openMenu) => <Toolbar title={title} leftToolbar={leftToolbar(openMenu)}/>;
 
 const createMenu = trans => [{
     title: trans.SEARCH,
@@ -58,18 +50,16 @@ const onInit = (genData, bookIndex) => () => {
 //    const getTitleFromPath = _path => menu.reduce((res, cur) => (
 //        _path === `/${cur.path}` ? cur.title : res
 //    ), '');
-const App = () =>{
+const App = () => {
     return (
-        <GromApp centered={false}>
-            <Header
-                direction="row"
-                justify="between"
-                large
-                pad={{ horizontal: 'medium' }}
-            >
-                <Title>Grommet standalone</Title>
-            </Header>
-        </GromApp>
+        <Page
+            centered={false}
+        >
+            <Toolbar
+                title="Test1234"
+                clickIcon={() => {}}
+            />
+        </Page>
     );
 };
 
@@ -97,33 +87,33 @@ const App = () =>{
 //</Splitter>
 
 //
-//App.propTypes = {
+App.propTypes = {
 //    bookIndex: PropTypes.object,
 //    children: PropTypes.element,
-////    closeMenu: PropTypes.func.isRequired,
-////    generateData: PropTypes.func.isRequired,
-////    isOpen: PropTypes.bool.isRequired,
-////    openMenu: PropTypes.func.isRequired,
+//    closeMenu: PropTypes.func.isRequired,
+//    generateData: PropTypes.func.isRequired,
+//    isOpen: PropTypes.bool.isRequired,
+    openMenu: PropTypes.func.isRequired,
 //    path: PropTypes.string,
 //    routerPush: PropTypes.func,
-////    searchData: PropTypes.func.isRequired,
+//    searchData: PropTypes.func.isRequired,
 //    translate: PropTypes.object,
-//};
+};
 
-//const mapStateToProps = state => ({
+const mapStateToProps = state => ({
 //    bookIndex: state.resources.bookIndex,
 //    isOpen: state.app.isOpen,
 //    path: state.routing.locationBeforeTransitions.get('pathname'),
 //    translate: state.translation.translate.HEADLINES,
-//});
-//const actionList = {
+});
+const actionList = {
 //    closeMenu: appActions.closeMenu,
 //    generateData: resourcesActions.generateData,
-//    openMenu: appActions.openMenu,
+    openMenu: appActions.openMenu,
 //    routerPush: routerActions.push,
 //    searchData: resourcesActions.searchData,
 //
-//};
+};
 
-//export default connector(mapStateToProps, actionList)(App);
-export default App;
+export default connector(mapStateToProps, actionList)(App);
+//export default App;
