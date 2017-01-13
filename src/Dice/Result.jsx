c9f8d4313c6f75a2599acb0f482ac716fc4b6020
Salztorte <olli.franzen@gmx.de>
2017-01-13 14:36:23 +0100

    ...

--- src/Dice/Result.jsx
+++ src/Dice/Result.jsx
@@ -5,36 +5,38 @@ import Box from 'grommet/components/Box';
 import SVGIcon from 'grommet/components/SVGIcon';
 import BoxItem from '../Componets/BoxItem.jsx';
 
-import { Zero, One, Two, Three, Four, Five, Six } from '../Resources/Dice';
-
-const Icon: Function[] = [
-    <Zero />,
-    <One />,
-    <Two />,
-    <Three />,
-    <Four />,
-    <Five />,
-    <Six />,
-];
+//import { Zero, One, Two, Three, Four, Five, Six } from '../Resources/Dice';
+//
+//const Icon: Function[] = [
+//    <Zero />,
+//    <One />,
+//    <Two />,
+//    <Three />,
+//    <Four />,
+//    <Five />,
+//    <Six />,
+//];
 const Result: Function = ({
     rollResult,
     }) =>
     (
         <BoxItem>
-            <Box
-                direction='row'
-            >
 
-                {
-                    rollResult.map((cur, i) => (
-                        <SVGIcon key={i} viewBox='0 0 130 108' type='logo'>
-                            {Icon[cur]}
-                        </SVGIcon>
-                    ))
-                }
-            </Box>
         </BoxItem>
     );
+
+//<Box
+//    direction='row'
+//>
+//
+//    {
+//        rollResult.map((cur, i) => (
+//            <SVGIcon key={i} viewBox='0 0 130 108' type='logo'>
+//                {Icon[cur]}
+//            </SVGIcon>
+//        ))
+//    }
+//</Box>
 
 Result.propTypes = {
     rollResult: PropTypes.arrayOf(PropTypes.number),
