9d3e69d333a68f4d17f4e3645854d300128ea87b
Salztorte <olli.franzen@gmx.de>
2017-01-13 16:12:43 +0100

    ...

--- src/Initative/Initative.jsx
+++ src/Initative/Initative.jsx
@@ -10,7 +10,7 @@ import NewEntry from './NewEntry.jsx';
 import SetIni from './SetIni.jsx';
 import List from './List.jsx';
 
-import { isOpen as isOpenType, isError as isErrorType, Entry as EntryType } from './InitState';
+import { isOpen as isOpenType, isError as isErrorType, Entry as EntryType } from './InitState.js';
 
 import { actions } from './Actions';
 
