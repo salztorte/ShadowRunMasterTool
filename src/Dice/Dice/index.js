1d4a07d6f9a64e073000a88c020f85ce47423d83
Salztorte <olli.franzen@gmx.de>
2017-01-13 14:51:20 +0100

    ...

---
+++ src/Dice/Dice/index.js
@@ -0,0 +1,9 @@
+import Zero from './zero.svg';
+import One from './one.svg';
+import Two from './two.svg';
+import Three from './three.svg';
+import Four from './four.svg';
+import Five from './five.svg';
+import Six from './six.svg';
+
+export { Zero, One, Two, Three, Four, Five, Six };
